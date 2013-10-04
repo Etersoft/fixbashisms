51f2f1d192ef95dcfb10ea397814bdd4cd68d5c7
Vitaly Lipatov <lav@etersoft.ru>
2013-10-04 16:19:52 +0400

    initial commit fixbashisms (copied from etersoft-build-utils)

---
+++ fixbashisms.sh
@@ -0,0 +1,37 @@
+# http://mywiki.wooledge.org/Bashism
+# see checkbashisms
+
+fatal()
+{
+	echo "FATAL: $@"
+	exit 1
+}
+
+remove_bashisms()
+{
+	local FILENAME="$1"
+	test -w "$FILENAME" || fatal "File '$FILENAME' is missed or read only"
+
+	cp $FILENAME ${FILENAME}~
+
+	subst "s|^pushd \(.*\)|cd \1 >/dev/null|g" $FILENAME
+	subst "s|^popd|cd - >/dev/null|g" $FILENAME
+
+	# {1,2} translation
+	# FIXME: miss first spaces
+	while read -r n ; do
+		echo "$n" | grep -v "{.*}" && continue
+		rs="$(echo "$n" | perl -pe "s|.*\s(.*?{.*?}.*?)\s.*|\1|g" )"
+		res=$(eval echo "$rs")
+		echo "$n" | perl -pe "s|$rs|$res|g"
+	done < $FILENAME >$FILENAME.tmp
+
+	[ -s "$FILENAME.tmp" ] && mv -f $FILENAME.tmp $FILENAME
+
+}
+
+for i in $@ ; do
+	remove_bashisms $i
+	#TODO: verbose
+	#checkbashisms $SPECNAME
+done
---
+++ test_remove_bashism.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+SPEC=get_ver.spec
+
+gen_spec()
+{
+cat <<EOF >$SPEC
+Name: get_version_test
+Release: alt1
+Version: $TESTVER
+Source: $SOURCEPATH%name-%version.tar.bz2
+Summary: Test
+Group: Other
+License: Public License
+%define major 1.0
+%define ver 10
+%define maj 1.0
+
+%build
+pushd txt
+echo {1,2}text
+%__subst "s|1|2|g" text/{1,eweew}
+%__subst "s|1re er| erer2|g" text/{1,eweew} nono
+mkdir ${LOCATION}/{loc1,dedt,ohi}
+mkdir $LOCATION/{loc1,dedt,ohi}
+popd
+
+%description
+Get version test
+%changelog
+* Date
+- Hello
+  * dsdkljd
+* Date
+
+EOF
+}
+
+TESTVER=0
+SOURCEPATH=ftp://etersoft.ru/pub/Etersoft/TEST/
+gen_spec
+./fixbashisms.sh $SPEC
+mv $SPEC $SPEC.new
+gen_spec
+diff -u $SPEC $SPEC.new
